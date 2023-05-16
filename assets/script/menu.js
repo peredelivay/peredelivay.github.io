5b61ba39e56e08d9d82b65535cbd4e4aed5d4372
peredelivay <90677290+peredelivay@users.noreply.github.com>
2023-05-16 04:15:53 +0300

    Delete assets directory

--- assets/script/menu.js
+++
@@ -1,7 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
-
-menuToggle.addEventListener("click", () => {
-  menuToggle.classList.toggle("active");
-  menu.classList.toggle("active");
-});
